b12ad2b48427a40003aac0cd517d3a0580214ea8
Thandile Nododile <nododilereply@gmail.com>
2020-10-12 06:05:27 +0200

    Updated commits

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Gues the number!");
+    println!("Please input your guess.");
+    //create a mutable string guess and pass the instance of a new string
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line"); //read an input from a user, pass it as a mutable reference 
+        //& handle potential failer of the program
+    println!("You guessed: {}", guess); //print the value of guess
 }
+
