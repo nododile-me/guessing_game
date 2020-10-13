3fd667a124c178b7c128d3513f8f698c961d58fe
Thandile Nododile <nododilereply@gmail.com>
2020-10-13 11:44:09 +0200

    Finalized the guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,42 @@
-//importing the io library from the stardand library
-use std::io;
+use std::io; //importing the standard library
+use rand::Rng; //importing the rand library 
+use std::cmp::Ordering; //importing the Ordering
 
 fn main() {
-    println!("Gues the number!");
-    println!("Please input your guess.");
-    //create a mutable string guess and pass the instance of a new string
-    let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line"); //read an input from a user, pass it as a mutable reference 
-        //& handle potential failer of the program
-    println!("You guessed: {}", guess); //print the value of guess
+    println!("Guess the number!");
+
+    //create a variable secret_number and generate a random number between 1 and 100 and assign into that varibale
+    let secret_number  = rand::thread_rng().gen_range(1, 101);
+    
+    loop{ 
+        println!("Please input your guess.");
+    
+        //create a mutable string guess and pass the instance of a new string
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line"); //read an input from a user, pass it as a mutable reference 
+            //& handle potential failer of the program
+    
+        //u32 - unassigned 32 bit integer        
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        }; //check if user input can be converted into a number if not then continue the loop
+        
+        
+        println!("You guessed: {}", guess); //print the value of guess
+
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;  //break the game if user guess is correct
+            }
+        }
+
+    }
 }
-
